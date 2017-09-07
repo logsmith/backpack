e918dc6485d4e7c12bc73be973691bd0f3da89d7
David Darke <david@atomicsmash.co.uk>
2017-09-07 21:53:46 +0100

    Started adding better colours and variable checking

--- Readme.md
+++ Readme.md
@@ -1,3 +1,10 @@
 ```
 bash /var/www/www.website.co.uk/vendor/logsmith/backpack/backup.sh
 ```
+
+## To install S3CMD run:
+
+```
+sudo apt-get install python-pip
+sudo pip install s3cmd
+```
--- backup.sh
+++ backup.sh
@@ -1,29 +1,66 @@
 #!/bin/bash
 
+red=`tput setaf 1`
+grn=`tput setaf 2`
+blu=`tput setaf 4`
+mag=`tput setaf 5`
+end=`tput sgr0`
+
 # Check to see if s3cmd is installed
 type s3cmd >/dev/null 2>&1 || { echo >&2 "I require s3cmd but it's not installed."; exit 1; }
 
 # cd to where THIS script is located
 cd ${0%/*}
-cd ../../../
+# cd ../../../
+cd ../atomicsmash.dev/
 
 # Example command
 WPDBNAME=`cat active-config.php | grep DB_NAME | cut -d \' -f 4`;
-BUCKET_NAME=`cat active-config.php | grep S3_BACKUP_BUCKET_NAME | cut -d \' -f 4`
-REGION=`cat active-config.php | grep S3_BACKUP_REGION | cut -d \' -f 4`
-ACCESS_KEY=`cat active-config.php | grep S3_BACKUP_ACCESS_KEY | cut -d \' -f 4`
-SECRET_KEY=`cat active-config.php | grep S3_BACKUP_SECRET_KEY | cut -d \' -f 4`
-SLACK_WEBHOOK_URL=`cat active-config.php | grep S3_BACKUP_SLACK_WEBHOOK_URL | cut -d \' -f 4`
+BUCKET_NAME=`cat active-config.php | grep BACKPACK_NAME | cut -d \' -f 4`
+REGION=`cat active-config.php | grep BACKPACK_REGION | cut -d \' -f 4`
+ACCESS_KEY=`cat active-config.php | grep BACKPACK_ACCESS_KEY | cut -d \' -f 4`
+SECRET_KEY=`cat active-config.php | grep BACKPACK_SECRET_KEY | cut -d \' -f 4`
+SLACK_WEBHOOK_URL=`cat active-config.php | grep BACKPACK_SLACK_WEBHOOK_URL | cut -d \' -f 4`
 
 filename=$WPDBNAME'--'$(date  +"%Y-%m-%d--%H-%M")'.sql';
 
+if [ -z $BUCKET_NAME ]; then
+	echo "${red}Bucket NAME missing from WP config${end} 🛠"
+fi
+
+if [ -z $BACKPACK_REGION ]; then
+	echo "${red}Bucket REGION missing from WP config${end} 🛠"
+fi
+
+if [ -z $BACKPACK_ACCESS_KEY ]; then
+	echo "${red}Bucket ACCESS_KEY missing from WP config${end} 🛠"
+fi
+
+if [ -z $BACKPACK_SECRET_KEY ]; then
+	echo "${red}Bucket SECRET_KEY missing from WP config${end} 🛠"
+fi
+
+
+if [[ -z $BUCKET_NAME || -z $BACKPACK_REGION || -z $BACKPACK_ACCESS_KEY || -z $BACKPACK_SECRET_KEY ]]; then
+	echo "--------------------------------------------------------------------------"
+	exit
+fi
+
+
 if [ ! -d "wp-content/backups" ]; then
+	echo "${grn}Making new backup directory${end} 🛠"
+	echo "--------------------------------------------------------------------------"
 	mkdir wp-content/backups
 fi
 
 
+# echo $filename;
+# exit
+
 # Back up the WordPress database with WP-CLI
 wp db export wp-content/backups/auto-database-backup.sql --allow-root
+
+
 
 # exit 1;
 
